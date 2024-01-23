48cf43f25479ede594c5c3dda8718de5270f9d22
Cristhian Cruz <c.cruzcoro73@gmail.com>
2024-01-23 02:42:47 -0500

    AU-319 Splash and Welcome screens

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,5 +1,6 @@
 import 'package:go_router/go_router.dart';
 import 'package:wabu/features/authentication/presentation/screens/splash_screen.dart';
+import 'package:wabu/features/authentication/presentation/screens/welcome_screen.dart';
 
 final appRouter = GoRouter(
   initialLocation: SplashScreen.route,
@@ -9,6 +10,13 @@ final appRouter = GoRouter(
       path: SplashScreen.route,
       builder: (context, state) {
         return const SplashScreen();
+      },
+    ),
+    GoRoute(
+      name: WelcomeScreen.name,
+      path: WelcomeScreen.route,
+      builder: (context, state) {
+        return WelcomeScreen();
       },
     ),
   ],
