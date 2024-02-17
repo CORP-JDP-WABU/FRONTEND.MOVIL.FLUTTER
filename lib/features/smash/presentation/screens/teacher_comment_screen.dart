6c4c9b48833c420528d53d13a33f7895318da8d9
Cristhian Cruz <c.cruzcoro73@gmail.com>
2024-02-17 00:36:58 -0500

    fix: Integration

--- lib/features/smash/presentation/screens/teacher_comment_screen.dart
+++ lib/features/smash/presentation/screens/teacher_comment_screen.dart
@@ -41,12 +41,14 @@ class _TeacherRequiredRatingContent extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final state = ref.watch(teachersTinderControllerProvider);
     final smashSuggestion = state.selectedSmashSuggestion;
-    final TextEditingController _commentController = TextEditingController();
+    final commentController = TextEditingController();
+
     WidgetsBinding.instance.addPostFrameCallback((_) {
       if (state.qualificationStatus == TeacherQualificationStatus.loaded) {
         showConfirmationDialog(context);
       }
     });
+
     return LayoutBuilder(
       builder: (context, contraints) {
         final maxHeight = contraints.maxHeight;
@@ -134,7 +136,7 @@ class _TeacherRequiredRatingContent extends ConsumerWidget {
                               padding: const EdgeInsets.all(10.0),
                               child: TextField(
                                 style: const TextStyle(fontSize: 11),
-                                controller: _commentController,
+                                controller: commentController,
                                 decoration: const InputDecoration(
                                   hintText: 'Deja tu comentario aquí',
                                   hintStyle: TextStyle(fontSize: 11),
@@ -154,9 +156,8 @@ class _TeacherRequiredRatingContent extends ConsumerWidget {
                           ],
                         ),
                       ),
-                     
                       Padding(
-                          padding: EdgeInsets.all(10),
+                          padding: const EdgeInsets.all(10),
                           child: Row(
                               mainAxisAlignment: MainAxisAlignment.spaceBetween,
                               children: [
@@ -170,10 +171,10 @@ class _TeacherRequiredRatingContent extends ConsumerWidget {
                                           fontFamily: 'GothamRounded'),
                                     ),
                                     onTap: () {
-                                         ref
-                                        .read(teachersTinderControllerProvider
-                                            .notifier)
-                                        .submitQualification();
+                                      ref
+                                          .read(teachersTinderControllerProvider
+                                              .notifier)
+                                          .submitQualification();
                                     }),
                                 CustomFilledButton(
                                   text: 'PUBLICAR',
@@ -211,204 +212,11 @@ class _TeacherRequiredRatingContent extends ConsumerWidget {
         );
       },
     );
-    /* return Scaffold(
-      body: SingleChildScrollView(
-          child: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              Color.fromRGBO(130, 55, 243, 1.000),
-              Color.fromRGBO(226, 83, 166, 1.000),
-              Color.fromRGBO(251, 225, 155, 1.000),
-            ], // Cambia los colores según tu preferencia
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-          ),
-        ),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const SizedBox(height: 50),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  CustomBackButton(
-                    color: Colors.white,
-                    onTap: () => context.pop(),
-                  ),
-                  IconButton(
-                    icon: SvgPicture.asset('assets/images/svgs/menu.svg'),
-                    color: Colors.white,
-                    onPressed: () => {},
-                  ),
-                ],
-              ),
-              const SizedBox(height: 16),
-              Material(
-                shape: const CircleBorder(
-                    side: BorderSide(color: Colors.white, width: 3)),
-                child: CircleAvatar(
-                  radius: 72,
-                  backgroundImage: NetworkImage(
-                      smashSuggestion?.teacher?.photoUrl ?? '',
-                      scale: 144),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 16, right: 16, bottom: 32),
-                child: Container(
-                  decoration: const BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.vertical(
-                          top: Radius.circular(25),
-                          bottom: Radius.circular(25))),
-                  child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const SizedBox(height: 16),
-                        const SizedBox(height: 10),
-                        Text(
-                          smashSuggestion?.teacher?.lastName ?? '',
-                          style: const TextStyle(
-                            color: Color.fromRGBO(2, 51, 106, 1.000),
-                            fontFamily: 'SFProDisplay',
-                            fontSize: 20,
-                            height: 31 / 23,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        Text(
-                          smashSuggestion?.teacher?.firstName ?? '',
-                          style: const TextStyle(
-                            color: Color.fromRGBO(2, 51, 106, 1.000),
-                            fontFamily: 'SFProDisplay',
-                            fontSize: 20,
-                            height: 31 / 23,
-                            fontWeight: FontWeight.w400,
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(16),
-                          child: Text(
-                            smashSuggestion?.course?.name ?? '',
-                            style: const TextStyle(
-                              color: Color.fromRGBO(2, 51, 106, 1.000),
-                              fontFamily: 'SFProDisplay',
-                              fontSize: 17,
-                              height: 31 / 23,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            SvgPicture.asset(
-                                'assets/images/svgs/Vector 13.svg'),
-                            const Text(
-                              '  INGRESA UN COMENTARIO  ',
-                              textAlign: TextAlign.center,
-                              style: TextStyle(
-                                fontSize: 15,
-                                fontWeight: FontWeight.w700,
-                                color: AppTheme.courseNameColor,
-                              ),
-                            ),
-                            SvgPicture.asset(
-                                'assets/images/svgs/Vector 13.svg'),
-                          ],
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(22.0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.stretch,
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              Card(
-                                  child: Padding(
-                                padding: const EdgeInsets.all(12.0),
-                                child: TextField(
-                                  controller: _commentController,
-                                  decoration: const InputDecoration(
-                                    hintText: 'Deja tu comentario aquí',
-                                  ),
-                                  maxLines: 7,
-                                  maxLength: 280,
-                                  maxLengthEnforcement:
-                                      MaxLengthEnforcement.enforced,
-                                  onChanged: (value) {
-                                    ref
-                                        .read(teachersTinderControllerProvider
-                                            .notifier)
-                                        .setComment(value);
-                                  },
-                                ),
-                              )),
-                              const SizedBox(height: 6.0),
-                            ],
-                          ),
-                        ),
-                        const SizedBox(height: 30),
-                        Padding(
-                            padding: EdgeInsets.all(15),
-                            child: Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  InkWell(
-                                    child:const Text(
-                                      'SALIR SIN COMENTAR',
-                                      style: TextStyle(
-                                          color: Colors.grey,
-                                          decoration: TextDecoration.underline,
-                                          fontFamily: 'GothamRounded'),
-                                    ),
-                                    onTap: () {          
-                                     // context.pushNamed(SmashView.name);
-                                    }
-                                  ),
-                                  CustomFilledButton(
-                                    text: 'PUBLICAR',
-                                    textColor: Colors.white,
-                                    verticalPadding: 8,
-                                    linearGradient: const LinearGradient(
-                                      colors: [
-                                        AppTheme.linearGradientLight,
-                                        AppTheme.linearGradientDark
-                                      ],
-                                      begin: Alignment.topCenter,
-                                      end: Alignment.bottomCenter,
-                                      stops: [0.0, 1.0],
-                                    ),
-                                    onPressed: () {
-                                      ref
-                                          .read(teachersTinderControllerProvider
-                                              .notifier)
-                                          .submitQualification();
-
-                                      // TODO: ESTA LOGICA VA SOBRE EL WIDGET DE TEXTO
-                                      // final text = _commentController.text;
-                                      // if (text.isEmpty || text.length >= 20) {
-                                      // } else {}
-                                    },
-                                  ),
-                                ])),
-                        const SizedBox(height: 30),
-                      ]),
-                ),
-              ),
-            ],
-          ),
-        ),
-      )),
-    );*/
   }
 
   void showConfirmationDialog(BuildContext context) {
     showDialog(
       context: context,
-      barrierDismissible: false,
       builder: (BuildContext context) {
         return AlertDialog(
           content: Column(
@@ -435,8 +243,12 @@ class _TeacherRequiredRatingContent extends ConsumerWidget {
           ),
         );
       },
-    ).then((value){
-      Navigator.pop(context);
+    ).then((value) {
+      context.pop();
+      if (ModalRoute.of(context)?.settings.name == SmashView.name) return;
+      context.pop();
+      if (ModalRoute.of(context)?.settings.name == SmashView.name) return;
+      context.pop();
     });
   }
 }
